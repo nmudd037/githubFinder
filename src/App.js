109fe12dbd6b498cf01c191da9a773bb7f922c5a
nmudd037 <nithishreddymuddasani.7@gmail.com>
2021-02-24 21:01:36 -0500

    Change folder structure for context

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import './App.css';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import AlertState from './components/context/alert/AlertState';
-import GithubState from './components/context/github/GithubState';
 import Alert from './components/layout/Alert';
 import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar';
@@ -11,6 +9,8 @@ import About from './components/pages/About';
 import Home from './components/pages/Home';
 import NotFound from './components/pages/NotFound';
 import User from './components/users/User';
+import AlertState from './context/alert/AlertState';
+import GithubState from './context/github/GithubState';
 
 const App = () => {
   return (
